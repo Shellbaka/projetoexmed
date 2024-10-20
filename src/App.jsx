cfe5baf62f1d08110c6daa185006f5869d1061be
renanmirandaa99 <renanmirandaa99@gmail.com>
2024-10-20 19:08:45 +0000

    Criação da página de exames começando, trazendo duas imagens e algumas informações de pesquisa e de exames e buscas.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
+import Exame from './pages/Exames/Exame'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <Header/>
-      <Home></Home>
+      <Exame></Exame>
       <Footer/>
     </>
   )
---
+++ src/pages/Exames/Exame.jsx
@@ -0,0 +1,36 @@
+import './Exame.css';
+
+
+export default function Exame() {
+return (
+
+<div className='Exame'>
+<div className='botao1'>
+    <button>Agendar Novo Exame</button>
+</div>
+
+
+
+<div className='Agendar'>
+    <img className='Agenda' src="https://veja.abril.com.br/wp-content/uploads/2017/05/mulher-fazendo-exame-20150316-001.jpg?quality=70&strip=info&w=1280&h=720&crop=1" alt="Solicitar Exame"></img>
+</div>
+
+<div className='botao2'>
+    <button>Buscar Resultado</button>
+</div>
+
+
+<div className='Buscar'>
+<img className='Busca' src="https://www.qvsaude.com.br/wp-content/uploads/elementor/thumbs/exames-de-rotina-pofghcnvlxephxjf2r5mg7955jctuxqutrokodsia0.jpg"alt="Resultado Exames"></img>
+</div>
+
+
+
+
+
+</div>
+
+
+);
+};
+
