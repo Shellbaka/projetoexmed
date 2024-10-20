01d34562fdd4109330da8bac8d9e9f9f373ae82b
renanmirandaa99 <renanmirandaa99@gmail.com>
2024-10-20 14:21:55 +0000

    Algumas modificações foram feitas, tais elas como a criação da página home e a sua estilização, falta ainda modelar como parágrafo e acertar a posição em relação a tela assim como mudar a fonte utilizada nos textos.

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,35 @@
+import './Home.css';
+
+export default function Home () {
+return (
+
+<div className="home">
+ 
+<h2 className='preto'>
+ Cuidar da sua saúde 
+</h2>
+<h2 className="vermeio">nunca foi tão fácil</h2>
+<p>
+Tenha acesso a uma nova experiência 
+de cuidados com a 
+saúde a partir de R$ 19,90/mês.
+</p>
+
+<button className='Seja'>
+    <a>
+        Seja Exmed
+    </a>
+</button>
+
+<div>
+
+<img className='ExmedP' src="https://www.valencis.com.br/wp-content/uploads/2017/05/Enfermagem-1.png" alt="Paciente Exmed" />
+</div>
+
+
+</div>
+
+
+);
+
+};
