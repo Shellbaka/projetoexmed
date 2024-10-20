7361ae4240b53c6482c915843b6c89ee49b1db32
renanmirandaa99 <renanmirandaa99@gmail.com>
2024-10-20 23:51:22 +0000

    Terminei mais algumas estilizações da página de Exames, acertei o posicionamento dos botões, Coloquei diminuição de opacidade das imagens ao passar mouse por cima, bordas interativas e imagens clicáveis. Depois irei implementar o clique com direcionamento nos botões também.

--- src/pages/Exames/Exame.jsx
+++ src/pages/Exames/Exame.jsx
@@ -6,13 +6,15 @@ return (
 
 <div className='Exame'>
 <div className='botao1'>
-    <button>Agendar Novo Exame</button>
+    <button>Agendar Exame</button>
 </div>
 
 
 
 <div className='Agendar'>
+    <a href='#Agendamento'>
     <img className='Agenda' src="https://veja.abril.com.br/wp-content/uploads/2017/05/mulher-fazendo-exame-20150316-001.jpg?quality=70&strip=info&w=1280&h=720&crop=1" alt="Solicitar Exame"></img>
+    </a>
 </div>
 
 <div className='botao2'>
@@ -21,7 +23,9 @@ return (
 
 
 <div className='Buscar'>
+    <a href='#Resultado'> 
 <img className='Busca' src="https://www.qvsaude.com.br/wp-content/uploads/elementor/thumbs/exames-de-rotina-pofghcnvlxephxjf2r5mg7955jctuxqutrokodsia0.jpg"alt="Resultado Exames"></img>
+    </a>
 </div>
 
 
