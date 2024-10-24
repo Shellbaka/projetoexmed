3c06a8149ccc7e88ccfed8aae7035a39719bc795
renanmirandaa99 <renanmirandaa99@gmail.com>
2024-10-24 22:18:06 +0000

    re roll

---
+++ src/pages/Loguin/Loguin.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import './Cadastro.css';
+
+function Cadastro() {
+  const [formData, setFormData] = useState({
+    nome: '',
+    endereco: '',
+    cpf: '',
+    contato: '',
+    email: '',
+    senha: '',
+    confirmarSenha: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica de envio do formulário
+    console.log(formData);
+  };
+
+  return (
+    <div className="container">
+      <div className="avatar">
+        <img src="public/logoexmed.svg" alt="Avatar" />
+      </div>
+      <form onSubmit={handleSubmit} className="form">
+        <h2>Cadastro</h2>
+        <label>
+          Nome:
+          <input
+            type="text"
+            name="nome"
+            value={formData.nome}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        
+        <label>
+          CPF:
+          <input
+            type="text"
+            name="cpf"
+            value={formData.cpf}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        
+        <label>
+          E-mail:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label>
+          Senha:
+          <input
+            type="password"
+            name="senha"
+            value={formData.senha}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label>
+          Confirmar Senha:
+          <input
+            type="password"
+            name="confirmarSenha"
+            value={formData.confirmarSenha}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Loguin;
